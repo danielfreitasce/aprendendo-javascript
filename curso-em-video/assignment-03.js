ae36acb60bdbf9f1d2fc428f9facff6c68328aae
Daniel Freitas <contatodanielfreitasce@gmail.com>
2021-07-26 22:32:20 -0300

    Adição de exercicios

---
+++ curso-em-video/assignment-03.js
@@ -0,0 +1,4 @@
+var nome = window.prompt('Qual é seu nome?')
+document.write(`Olá, ${nome}! Seu nome tem ${nome.length} letras.`)
+document.write(`Seu nome em maiúscula fica assim ${nome.toUpperCase()}.`)
+document.write(`Seu nome em minúscula fica assim ${nome.toLowerCase()}. `)
