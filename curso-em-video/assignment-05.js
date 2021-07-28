dee38a7091e9a79226a897d2543c71693a23d8bc
Daniel Freitas <contatodanielfreitasce@gmail.com>
2021-07-27 22:43:36 -0300

    atividade

---
+++ curso-em-video/assignment-05.js
@@ -0,0 +1,12 @@
+
+function somar(){
+    let tn1 = window.document.getElementById('txtn1')
+    let tn2 = window.document.getElementById('txtn2')
+    let res = window.document.getElementById('res')
+
+    let n1 = Number(tn1.value)
+    let n2 = Number(tn2.value)
+    let s = n1+n2
+
+    res.innerHTML = `A soma entre ${n1} e ${n2} é igual a <strong>${s}</strong>`
+}
