bbe8d4e595fb8e175b854d858e37de64e184f4ed
Daniel Freitas <contatodanielfreitasce@gmail.com>
2021-08-01 19:59:09 -0300

    Atividade de hoje

--- curso-em-video/assignment-09/assignment-09.js
+++ curso-em-video/assignment-09/assignment-09.js
@@ -1,19 +1,51 @@
-function contar(){
-   
+function contar() {
+
     let ini = window.document.getElementById('inicio')
     let fim = window.document.getElementById('fim')
     let passo = window.document.getElementById('passo')
+    let res = window.document.getElementById('res')
+    
+    if (ini.value.length == 0 || fim.value.length == 0 || passo.value.length == 0) {
+        res.innerHTML = `Impossível contar`
+
+    } else {
+
+        let i = Number(ini.value)
+        let f = Number(fim.value)
+        let p = Number(passo.value)
+
+        if(p <=0){
+            window.alert("Passo inválido")
+
+        }else if(i < f){
+            //contagem crescente
+            for (i ; i <= f; i += p) {
+
+                res.innerHTML += ` ${i} &#x1F449`
+                console.log = `${i} `
+     
+            }
+
+            res.innerHTML += `&#x1F3C1`
+
+        }else{
+            //contagem descrecente
+            for (i ; i >= f; i -= p) {
+
+                res.innerHTML += ` ${i} &#x1F449`
+                console.log = `${i} `
+     
+            }
+
+            res.innerHTML += `&#x1F3C1`
+        }
         
-    res.innerHTML = `Contando:<br>`
 
-    for(let i = Number(ini.value); i <= Number(fim.value);  i+=Number(passo.value)){
-
-        res.innerHTML += `${i} &#x1F449`
         
     }
 
-    res.innerHTML += `&#x1F3C1`
 
 
-        
+
+
 }
