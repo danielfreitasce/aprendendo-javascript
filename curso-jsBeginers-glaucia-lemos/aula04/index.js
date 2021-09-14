84e66578f6f9cafc93c1583b0a77c013668cfd64
Daniel Freitas <contatodanielfreitasce@gmail.com>
2021-09-14 18:54:50 -0300

    adicao de questoes

---
+++ curso-jsBeginers-glaucia-lemos/aula04/index.js
@@ -0,0 +1,10 @@
+console.log("Ola mundo")
+
+
+//templates literais
+let lugar = "Mundo"
+let saudacao = "Olá"
+
+console.log(`${saudacao} ${lugar}`)
+
+//Video 04 - Criando a sua Primeira Aplicação em JavaScript - https://youtu.be/e5cEpHibGdA
