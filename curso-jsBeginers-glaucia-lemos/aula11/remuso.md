50f79153896b4da406cc7c901c3e0b2550894d17
Daniel Freitas <contatodanielfreitasce@gmail.com>
2021-09-18 19:52:26 -0300

    adicao de material aula 11

---
+++ curso-jsBeginers-glaucia-lemos/aula11/remuso.md
@@ -0,0 +1,6 @@
+Template Literals
+
+É uma forma melhor de imprimir conteúdo. A sintaxe torna as strings mais fáceis de formatar e ler. 
+Usamos o marcador ${} para imprimir variáveis ou expressões. 
+
+[Usando Template Literals para formatar Strings | Série para Iniciantes em JavaScript #11 (1m23s)](https://www.youtube.com/watch?v=T_e6pdTaOX8)
---
+++ curso-jsBeginers-glaucia-lemos/aula12/templanteLiterals.js
@@ -0,0 +1,38 @@
+/** Concatenando strings: ************************************************* */
+
+let str1 = 'JavaScript'
+let str2 = 'legal!'
+
+console.log(`Formatando strings em ${str1} é ${str2}`) //Formatando strings em JavaScript é legal!
+
+/** Pulando linhas: ************************************************* */
+
+console.log(`Podemos pular linhas
+
+
+
+
+
+
+dessa forma`)
+/** saida:Podemos pular linhas
+
+
+
+
+
+
+dessa forma*/
+
+/** Expressões booleanas: ************************************************* */
+let bool1 = true;
+console.log(`O oposto de true é ${!bool1}.`) //saida: O oposto de true é false.
+
+
+/** chamando funções: ************************************************* */
+
+const getValue = (num) => {
+    return num + num
+}
+
+console.log(`Resultado: ${getValue(10)}`) //saida:20
